6bedba82e6751ebee65d751eacaa319c2f2aeb77
stefanvanberkum <stefanvanberkum@gmail.com>
2021-06-26 12:20:07 +0200

    Updated files.

--- plotShelf.m
+++ plotShelf.m
@@ -4,9 +4,9 @@ close all
 
 %% Load the results.
 path = "C:/Users/Stefan van Berkum/git/HAPSA/HAPSA/Results/";
-filenames = ["APSA/CSV/30_240", "VIS/CSV/30_240_50.0", "VIS/CSV/30_240_100.0", "VIS/CSV/30_240_150.0", "VIS/CSV/30_240_200.0", "HAPSA/CSV/30_240_100.0_5.0E-4"];
+filenames = ["APSA/CSV/30_240", "VIS/CSV/30_240_50.0", "VIS/CSV/30_240_100.0", "VIS/CSV/30_240_150.0", "VIS/CSV/30_240_200.0", "HAPSA/CSV/30_240_100.0_0.001"];
 outpath = "C:/Users/Stefan van Berkum/Google Drive/Studie/Thesis/Figures/Colormaps/";
-outfiles = ["APSA_30_240", "VIS_30_240_50" , "VIS_30_240_100", "VIS_30_240_150", "VIS_30_240_200", "HAPSA_30_240_100_00005"];
+outfiles = ["APSA_30_240", "VIS_30_240_50" , "VIS_30_240_100", "VIS_30_240_150", "VIS_30_240_200", "HAPSA_30_240_100_0001"];
 
 %% Generate colormaps.
 x = [1, 3];
